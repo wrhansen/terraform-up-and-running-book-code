314e05c4ab8644f87d3151c84984a4b2b15f98f8
Wesley Hansen <wesrhansen@gmail.com>
2023-05-31 14:31:40 -0400

    fix error with previous chapter

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -46,3 +46,19 @@ data "aws_iam_policy_document" "cloudwatch_full_access" {
     resources = ["*"]
   }
 }
+
+
+# Conditionally give neo access cloudwatch IAM policies
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full_access" {
+  count = var.give_neo_cloudwatch_full_access ? 1 : 0
+
+  user       = aws_iam_user.example[0].name
+  policy_arn = aws_iam_policy.cloudwatch_full_access.arn
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
+  count = var.give_neo_cloudwatch_full_access ? 0 : 1
+
+  user       = aws_iam_user.example[0].name
+  policy_arn = aws_iam_policy.cloudwatch_read_only.arn
+}
