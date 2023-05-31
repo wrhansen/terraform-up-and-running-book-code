cad8a345e455f184d433b7864a52b84829d961d4
Wesley Hansen <wesrhansen@gmail.com>
2023-05-31 14:31:51 -0400

    oops wrong place

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -205,18 +205,3 @@ resource "aws_autoscaling_schedule" "scale_in_at_night" {
   recurrence             = "0 17 * * *"
   autoscaling_group_name = aws_autoscaling_group.example.name
 }
-
-# Conditionally give neo access cloudwatch IAM policies
-resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full_access" {
-  count = var.give_neo_cloudwatch_full_access ? 1 : 0
-
-  user       = aws_iam_user.example[0].name
-  policy_arn = aws_iam_policy.cloudwatch_full_access.arn
-}
-
-resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
-  count = var.give_neo_cloudwatch_full_access ? 0 : 1
-
-  user       = aws_iam_user.example[0].name
-  policy_arn = aws_iam_policy.cloudwatch_read_only.arn
-}
